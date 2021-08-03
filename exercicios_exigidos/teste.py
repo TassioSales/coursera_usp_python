a403eed5b0457ffb1177c7680c496fa9cca64f45
TassioSales <tassio.ljs@gmail.com>
2021-08-02 22:54:15 -0300

    commit inicial

--- exercicios_exigidos/teste.py
+++ exercicios_exigidos/teste.py
@@ -1,12 +1,15 @@
-a=0
-b = 2  
-c = 1
-
-if (a > 0):
-    if (b > 0):
-        print ("Tudo ok para decolagem!")
+terminou = False
+p = i = 0
+while (not terminou):
+    n = int(input("Digite um número, ou zero para terminar: "))
+    if n == 0:
+        terminou = True
     else:
-        print ("Tanque principal vazio; voando com combustível reserva!")
-else:
-    if (c > 0):
-        print ("Foguete não tem piloto, mas há outros foguetes disponíveis!")
+        if n % 2 == 0:
+            p = p + 1
+        else:
+            i = i + 1
+
+print ("P = ", p)
+print ("I = ", i)
+    
